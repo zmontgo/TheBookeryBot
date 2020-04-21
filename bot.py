53bb1f2110feea1f1a2fdc8a0765ab7f2ade11b1
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-04-21 16:08:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,17 @@
-require Discord
+import discord
 
-if __name__ == "__main__":
-    bot.run(auth.get('discord', 'TOKEN'))
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
